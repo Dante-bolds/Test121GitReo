9e4db2bda23a451ff17b7332f8e1bdbaeee98683
Dante-bolds <nq8925bn@go.minnstate.edu>
2025-02-02 13:08:15 -0600

    Program.cs

--- FourSistersandTheDuckBear/FourSistersandTheDuckBear/Program.cs
+++ FourSistersandTheDuckBear/FourSistersandTheDuckBear/Program.cs
@@ -7,12 +7,11 @@
             Console.WriteLine("How chocolate eggs have you gathered?");
             int eggCounter = Convert.ToInt32(Console.ReadLine());
 
-
             int forSisters = eggCounter / 4;
             int forDuckbear = eggCounter % 4;
 
-            Console.WriteLine("Each sister gets " + forSisters + ".");
-            Console.WriteLine("The duckbear gets " + forDuckbear + ".");
+            Console.WriteLine("Each sister will get: " + forSisters);
+            Console.WriteLine("While the duckbear will get: " + forDuckbear);
         }
     }
 }
